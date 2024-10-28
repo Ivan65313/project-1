438976a669cd2bb604b2f9b7b82b74776b3c0a27
Ivan <777@mail.ru>
2024-10-28 14:35:31 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,185 @@
+"use strict";
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// var name = 'Ivan';
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+
+
+
+// Урок 10                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+
+// // const bool = true;
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+
+
+// // const obj = {
+// //     name: "John",
+// //     age: 25,
+// //     isMarried: false
+// // };
+
+// // console.log(obj.name);
+// console.log(obj ['name']);
+
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp'];
+// console.log(arr[1]);
+
+
+
+
+
+// Урок 11                     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+/* const arr = [1, 2, 3];  */  /* массив - перечень информации по порядку */
+// const arr = ['a', 'b', 'c']
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c',
+//     abc: {
+//         def: {
+
+//         }
+//     }
+// };
+// const b = 'b';
+
+// /* console.log(arr[1]); */
+// /* console.log(arrObj[1]); */
+// /* arrObj.b = '1234'; */
+// arrObj['b'] = '1234';
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// /* const obj {a: 1, b: 2}; */     /* структура хранения данных в парном формате ключ - значение */
+// const obj = {
+//     'Anna': 500,
+//     'Alice': 800
+// };
+
+
+
+// Урок 12                          !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(typeof(answer));
+
+// const answers = [];          /* создали переменную */
+
+// answers[0] = prompt("Как ваше имя?", "");     /* заполняем переменную */
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+
+
+
+/* Урок 13                             !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);          /* бэктики!!!!!!!!!!!! */
+
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
+
+
+
+
+
+// Урок 14                                        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
+console.log(4 + +' - object');
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+// ++incr;
+// --decr;
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+console.log(2*4 === '8');
+
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked && isClose);
+
+console.log(isChecked || isClose);
+
+console.log(isChecked || !isClose);
+
+
+
+
+
+// Урок 15                                      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
